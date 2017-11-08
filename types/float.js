c1df58377157a43881ccf823e171437c2e20cd79
Zebulon Young <zebulonj@gmail.com>
2017-11-08 09:50:44 -0800

    Add FloatType.

---
+++ types/float.js
@@ -0,0 +1,3 @@
+export default {
+  resolve: ( source ) => Promise.resolve( source )
+};
--- types/index.js
+++ types/index.js
@@ -2,10 +2,12 @@ import ObjectType from './object';
 import ArrayType from './array';
 import StringType from './string';
 import IntType from './int';
+import FloatType from './float';
 
 export {
   ObjectType,
   ArrayType,
   StringType,
-  IntType
+  IntType,
+  FloatType
 };
