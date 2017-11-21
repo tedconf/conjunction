0ce0662108fee7baa320342246903a3c62c6a1f1
Zebulon Young <zebulonj@gmail.com>
2017-11-21 15:28:00 -0800

    Implement root normalization.

--- store/Normalizer.js
+++ store/Normalizer.js
@@ -1,10 +1,29 @@
 // @flow
+import { mapObject } from 'util/object';
+
 type Node = {
   id?: string
 };
 
+type RecordKey = string;
+
 type Selector = {
   key: string
+};
+
+type Record = {};
+
+type RecordReference = {
+  __ref: RecordKey
+}
+
+type RecordRepository = {
+  [key: string]: Record
+};
+
+type NormalizedResponse = {
+  ref: RecordReference,
+  records: RecordRepository
 };
 
 /**
@@ -15,14 +34,43 @@ type Selector = {
  * have a key of 'rel:root'.
  */
 
-export const ROOT_ID = 'rel:root';
+export const ROOT_ID = '__root';
 
-export const normalize = ( selector: Selector, node: Node ) => {
-  const { key } = selector;
+export const normalize = ( selector: Selector, node: Node ): NormalizedResponse => {
+  console.log( 'normalize():', selector, node );
+  const key = node.id || selector.key;
+  const ref = { __ref: key };
 
-  const root = { __ref: node.id || key };
+  const branches = mapObject( node, ( fieldNode, fieldName ) => {
+    if ( typeof fieldNode !== 'object' ) {
+      // TODO: This would be more precise if type was determined by the schema, rather than inspection of the node.
+      return {
+        ref: fieldNode,
+        records: {}
+      };
+    }
+    else if ( Array.isArray( fieldNode ) ) {
+      return {
+        ref: [],
+        records: {}
+      }
+    }
+    else {
+      return normalize({ key: `${ key }:${ fieldName }` }, fieldNode );
+    }
+  });
+
+  const fields = mapObject( branches, ({ ref }) => ref );
+
+  const record = {
+    __key: key,
+    ...fields
+  };
 
   return {
-    root
+    ref,
+    records: {
+      [key]: record
+    }
   };
 }
--- store/Normalizer.spec.js
+++ store/Normalizer.spec.js
@@ -7,7 +7,7 @@ import {
 } from './Normalizer';
 
 test.only( "connect/store/Normalizer.normalize()...", sub => {
-  sub.test( "...node with id.", assert => {
+  sub.test( "...root.", assert => {
     const data = {
       group: {
         id: 'R3JvdXA6MDAwMDE=',
@@ -15,11 +15,21 @@ test.only( "connect/store/Normalizer.normalize()...", sub => {
       }
     };
 
-    const { root } = normalize( { key: ROOT_ID }, data );
+    const { ref, records } = normalize( { key: ROOT_ID }, data );
 
-    const expectedRoot = { __ref: ROOT_ID };
+    const expectedRef = { __ref: ROOT_ID };
+    const expectedRoot = {
+      __key: ROOT_ID,
+      group: {
+        __ref: 'R3JvdXA6MDAwMDE='
+      }
+    };
 
-    assert.deepEqual( root, expectedRoot, 'The normalize() method should return the correct root.' );
+    assert.deepEqual( ref, expectedRef, 'The normalize() method should return the correct root.' );
+
+    assert.notEqual( typeof records[ROOT_ID], 'undefined', 'A root record should be entered.' );
+    assert.deepEqual( records[ROOT_ID], expectedRoot, 'The root record should include the correct data.' )
+
     assert.end();
   });
 });
