8b84fe92bc1136a1d8692464722c06b240c930ce
Zebulon Young <zebulonj@gmail.com>
2017-11-24 11:30:00 -0800

    Decode GUIDs for interaction with API.

--- types/GUIDType.js
+++ types/GUIDType.js
@@ -1,3 +1,18 @@
 export const GUIDType = typename => ({
   resolve: ( source ) => Promise.resolve( btoa( `${ typename }:${ source }` ) )
 });
+
+const decode = ( encodedId ) => {
+  const [type, id] = atob( encodedId ).split( ':' );
+
+  return {
+    type,
+    id
+  };
+};
+
+GUIDType.decode = decode;
+
+export const GUIDInputType = ({
+  resolve: id => decode( id ).id
+});
