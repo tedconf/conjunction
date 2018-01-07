dd61e8038c002931aeb281e90ebec39ebd0af556
Zebulon Young <zebulonj@gmail.com>
2018-01-06 22:49:47 -0800

    Add minified module to rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 import pkg from './package.json';
 
@@ -46,6 +47,26 @@ export default [
 				exclude: ['node_modules/**']
 			}),
 			commonjs()
+		]
+	},
+	{
+		input: 'src/index.js',
+		external: [
+			'prop-types',
+			'ramda',
+			'rxjs',
+			'react'
+		],
+		output: [
+			{ file: 'dist/conjunction.cjs.min.js', format: 'cjs' }
+		],
+		plugins: [
+			resolve(),
+			babel({
+				exclude: ['node_modules/**']
+			}),
+			commonjs(),
+			uglify()
 		]
 	}
 ];
