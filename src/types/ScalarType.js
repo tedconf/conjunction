9deb24c63fa32a6753f7e24a5e87f4bc49acbe4d
Zebulon Young <zebulonj@gmail.com>
2018-01-17 12:49:26 -0800

    Stub the scalar type (#13).

---
+++ src/types/ScalarType.js
@@ -0,0 +1,11 @@
+// The generic ScalarType is being added in connection with #13, but will necessarily
+// be accompanied by significant changes in the graph normalization algorithm. In the
+// context of Conjunction schema types, "scalar" refers to a leaf in the data graph.
+//
+// TODO: Most scalar types (e.g., StringType, BoolType,...) could be replaced with
+// aliases to this ScalarType, since we only mutate the data associated with a scalar
+// in rare cases (which would continue to have their own implementations).
+
+export const ScalarType = {
+  resolve: ( source ) => Promise.resolve( source )
+};
--- src/types/index.js
+++ src/types/index.js
@@ -3,6 +3,7 @@ import StringType from './string';
 import IntType from './int';
 import FloatType from './float';
 
+export * from './ScalarType';
 export * from './BoolType';
 export * from './ObjectType';
 export * from './InputObjectType';
