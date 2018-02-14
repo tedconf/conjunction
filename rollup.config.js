0da3a3e76b4fdaaa043e2d2db048b06a01811c44
Zebulon Young <zebulonj@gmail.com>
2018-02-13 22:16:20 -0800

    Update build to make babel-runtime a peer dependency.
    Including the runtime as a peer dependency (rather than baking it into this package), ensures that code from the runtime will not be duplicated if also a dependency of the project importing conjunction.

--- rollup.config.js
+++ rollup.config.js
@@ -5,47 +5,78 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default [
-	{
-		input: 'src/index.js',
-		external: [
-			'prop-types',
-			'react'
-		],
-		output: {
-			name: 'conjunction',
-			file: pkg.browser,
-			format: 'umd',
-			globals: {
-				'react': 'React',
-				'prop-types': 'PropTypes'
-			}
-		},
-		plugins: [
-			resolve(),
-			babel({
-				exclude: ['node_modules/**']
-			}),
-			commonjs()
-		]
-	},
-	{
-		input: 'src/index.js',
-		external: [
-			'prop-types',
-			'ramda',
-			'rxjs',
-			'react'
-		],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		],
-		plugins: [
-			resolve(),
-			babel({
-				exclude: ['node_modules/**']
-			}),
-			commonjs()
-		]
-	}
+  {
+    input: 'src/index.js',
+    external: [
+      'prop-types',
+      'react'
+    ],
+    output: {
+      name: 'Conjunction',
+      file: pkg.browser,
+      format: 'umd',
+      globals: {
+        'react': 'React',
+        'prop-types': 'PropTypes'
+      }
+    },
+    plugins: [
+      resolve(),
+      babel({
+        presets: [
+          "flow",
+          "react",
+          ["env", {
+            "targets": {
+              "browsers": ["last 2 versions", "safari >= 7"]
+            },
+            "modules": false
+          }],
+          "stage-1"
+        ],
+        plugins: [
+          "external-helpers"
+        ]
+      }),
+      commonjs()
+    ]
+  },
+  {
+    input: 'src/index.js',
+    external: ( id ) => {
+      const mods = [
+        'prop-types',
+        'react'
+      ];
+
+      if ( mods.indexOf( id ) !== -1 ) return true;
+
+      return !!id.match( /^babel-runtime/i );
+    },
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        runtimeHelpers: true,
+        presets: [
+          "flow",
+          "react",
+          ["env", {
+            "targets": {
+              "browsers": ["last 2 versions", "safari >= 7"]
+            },
+            "modules": false
+          }],
+          "stage-1"
+        ],
+        plugins: [
+          "transform-runtime"
+        ]
+      }),
+      commonjs()
+    ]
+  }
 ];
